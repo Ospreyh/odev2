3d21f23d8a0097eadfe999e1db0d0eca45d4eeb8
Ospreyh <113728917+Ospreyh@users.noreply.github.com>
2022-09-28 22:03:06 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/Person.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Person {
+
+	
+		int id;
+		String firstName;
+		String lastName;
+		int age;
+
+	
+
+}
---
+++ inheritanceDemo/src/inheritanceDemo/TarimKrediManager.java
@@ -0,0 +1,12 @@
+package inheritanceDemo;
+
+public class TarimKrediManager extends BaseKrediManager {
+	
+	//bir class sadece bir class'a extend edebilir
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
